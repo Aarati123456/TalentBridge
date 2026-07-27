cef023929d35c6163c76f645ed4bc3b8f8ce16f1
Aarati Limbu <aaratiangbuhang2058@gmail.com>
2026-07-27 15:43:39 +0545

    Update routing and homepage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import Upload from "./pages/Upload";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/upload" element={<Upload />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
 
-export default App;
+export default App;git status
