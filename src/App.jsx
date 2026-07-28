a5b379bdd3b958e612ec836b7da6eb473a031f80
Aarati Limbu <aaratiangbuhang2058@gmail.com>
2026-07-28 12:14:10 +0545

    Set up routing and home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import UploadTalent from "./pages/UploadTalent";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      {/* Navbar सबै pages मा देखिन्छ */}
-      <Navbar />
-
-      {/* URL अनुसार page change हुन्छ */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route path="/explore" element={<Explore />} />
-
-        <Route path="/upload" element={<UploadTalent />} />
-
-        <Route path="/dashboard" element={<Dashboard />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/register" element={<Register />} />
-      </Routes>
-
-      {/* Footer सबै pages मा देखिन्छ */}
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
