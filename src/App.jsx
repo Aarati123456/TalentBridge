da74162fae31fad68374f2408081bf78a9b32de3
Aarati Limbu <aaratiangbuhang2058@gmail.com>
2026-07-29 11:34:06 +0545

    Add routes for home and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Explore from "./pages/Explore";
+import Dashboard from "./pages/Dashboard";
+import UploadTalent from "./pages/UploadTalent";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/upload" element={<UploadTalent />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   );
