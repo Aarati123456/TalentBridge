df8716d3dff21cfd2403053d9d459d74414abc9c
Aarati Limbu <aaratiangbuhang2058@gmail.com>
2026-08-05 12:59:18 +0545

    Set up dashboard with cards and recent talents data

---
+++ src/components/DashboardCard.jsx
@@ -0,0 +1,4 @@
+import { Link } from "react-router-dom";
+import DashboardCard from "../components/DashboardCard";
+import RecentTalentCard from "../components/RecentTalentCard";
+import { stats, recentTalents } from "../data/dashboardData"
