d5d7b03c259e5c5a76d5e258760f671f8c092611
Aarati Limbu <aaratiangbuhang2058@gmail.com>
2026-08-12 23:11:14 +0545

    Upgrade dashboard layout and make navigation clickable

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Explore from "./pages/Explore";
 import Dashboard from "./pages/Dashboard";
 import UploadTalent from "./pages/UploadTalent";
-import Sidebar from "./components/Sidebar";
+
+import DashboardLayout from "./components/DashboardLayout";
 
 function App() {
   return (
     <Routes>
+      {/* Public Pages */}
       <Route path="/" element={<Home />} />
-      <Route path="/explore" element={<Explore />} />
-      <Route path="/upload" element={<UploadTalent />} />
-      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Pages with Sidebar */}
+      <Route element={<DashboardLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/upload" element={<UploadTalent />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
